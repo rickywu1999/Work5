7997f85e48bf20417e19497cba0fed915b3ebbea
rickywu1999 <rwu4@stuy.edu>
2016-11-02 14:49:43 -0400

    wrote recursion, pls bugix

--- dir.c
+++ dir.c
@@ -7,6 +7,38 @@
 #include <time.h>
 #include <dirent.h>
 
+
+//Note to Nancy: will probably not start this until 8 or 9. If you have time, bugfix this (could not compile at library) and work on printing out size of the files
+void print_dir(struct dirent ent, int level){
+  while (ent){
+    int c;
+    DIR *d;
+    struct dirent newent;
+    char n[] = ent->name;
+    printf("%s",n);
+    
+    if (ent->d_type == DT_REG){
+      //this part is literally for spacing and formatiting purposes
+      c =level;
+      while (c > 0){
+        printf("\n");
+        c++;
+      }
+      printf("file\n");
+    }
+    
+    if (ent->d_type == DT_DIR){
+      printf("directory\n");
+      //recursion is over here! basically if there is a directory, then use print_dir on the directory's ent
+      d = opendir(n);
+      newent = readdir(d);
+      print_dir(newent,level++);
+      closedir(d);
+    }
+    ent = readdir(d);
+  }
+}
+
 int main(){
   DIR *d = opendir("stuff");
   if (!d){
@@ -14,13 +46,7 @@ int main(){
     return 0;
   }
   struct dirent *ent = readdir (d);
-  while (ent){
-    printf("%s: ",ent->d_name);
-    if (ent->d_type == DT_REG)   
-      printf("file\n");
-    else
-      printf("directory\n");
-    ent = readdir(d);
-  }
+  print_dir(ent,0);
+  close_dir(d);
   return 0;
 }
